b1e0a587291456e1b5b05e770f2c5697aefe150d
Basila123 <basilanoushad92@gmail.com>
2021-10-26 22:26:20 +0530

    Added main method

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabzAddress;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to the Address Book");
+    }
+}
