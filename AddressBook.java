751b655dab343fa06773193cc8446a4b2377bbac
Basila123 <basilanoushad92@gmail.com>
2021-10-27 04:59:01 +0530

    Editing feature have added

--- AddressBook.java
+++ AddressBook.java
@@ -1,14 +1,12 @@
 package com.bridgelabzAddress;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBook {
     public static void main (String[] args){
         Scanner s= new Scanner(System.in);
         Scanner s1=new Scanner(System.in);
         // create instance collection to insert data and use array list collection
-        Collection<ContactPerson>collect=new ArrayList<ContactPerson>();
+        List<ContactPerson>collect=new ArrayList<ContactPerson>();
         int choice;
         //use do while concept to add choices.
         do{
@@ -45,7 +43,52 @@ public class AddressBook {
                     break;
                 case 2:
                     System.out.println(collect);
-            }
+                    break;
+                case 3 :
+                    boolean found=false;
+                    System.out.println("Enter first name to update");
+                    firstname=s.next();
+                    System.out.println("_______________________________________________________>");
+                    ListIterator<ContactPerson>li= collect.listIterator();//iterating using list iterator
+                    while(li.hasNext()){
+                        ContactPerson contact=li.next();
+                        if(contact.getFirstname()==firstname){
+                            System.out.println("Enter first name");
+                            firstname=s.next();
+                            System.out.println("Enter last name");
+                            lastname=s.next();
+                            System.out.println("Enter state");
+                            state=s.next();
+                            System.out.println("Enter zip");
+                            zip=s.next();
+                            System.out.println();
+                            address=s.next();
+                            System.out.println("Enter your city");
+                            city=s.next();
+                            System.out.println("Email");
+                            email=s.next();
+                            System.out.println("Enter your phone number");
+                            phno=s1.nextInt();
+                            System.out.println("Enter your employee number");
+                            employeno=s1.nextInt();
+                            li.set(new ContactPerson(employeno,firstname,lastname,address,city,state,zip,
+                                    email,phno));
+                            found=true;
+                        }
+                    }
+                    if(!found){
+                        System.out.println("Record Not Found");
+                    }
+                    else{
+                        System.out.println("Record is updated Successfully------------!");
+                    }
+                    System.out.println("______________________________________");
+                    break;
+                //case 4:
+                 }
+
+
+
 
         }while(choice!=0);
       /*
